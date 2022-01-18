ae65d89b4ab380e47b83b12b1147c00bdb84fee4
Daisy Guerrero <dguerrero1@bsd220.org>
2022-01-18 10:35:02 -0600

    Created Array

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var assignmentItems = [AssignmentItem(course: "Math", description: "Chapter 1 HW", dueDate: Date()), AssignmentItem(course: "Science", description: "Finish lab", dueDate: Date()), AssignmentItem(course: "Spanish", description: "Study vocab", dueDate: Date())]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach (assignmentItems) { item in
+                    Text(item.description)
+                    
+                }
+            }
+            .navigationBarTitle("Assignment Notebook")
+        }
     }
 }
 
@@ -18,4 +26,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct AssignmentItem: Identifiable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
 }
